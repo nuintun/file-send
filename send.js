5f9d36474b25922550d373a9552ed8a854cfcbee
Nuintun <nuintun@qq.com>
2014-12-08 23:33:59 +0800

    rewrite

--- send.js
+++ send.js
@@ -27,88 +27,82 @@ function Send(root, options){
   options = options || {};
 
   // Set options property
-  Object.defineProperty(this, 'options', {
-    __proto__: null,
-    writable: false,
-    enumerable: false,
-    configurable: false,
-    value: {
-      // Root
-      set root(root){
-        root = util.isType(root, 'string')
-          ? path.resolve(root)
-          : cwd;
+  util.privateProperty(this, 'options', {
+    // Root
+    set root(root){
+      root = util.isType(root, 'string')
+        ? path.resolve(root)
+        : cwd;
 
-        this._root = util.httpPath(root + path.sep);
-      },
-      get root(){
-        return this._root;
-      },
-      // Index
-      set index(index){
-        // Default document
-        this._index = index === false
-          ? []
-          : index === undefined || index === true
-          ? ['index.htm', 'index.html']
-          : util.normalizeList(index);
-      },
-      get index(){
-        return this._index;
-      },
-      // Extensions
-      set extensions(extensions){
-        this._extensions = !extensions
-          ? []
-          : util.normalizeList(extensions);
-      },
-      get extensions(){
-        return this._extensions;
-      },
-      // Etag
-      set etag(etag){
-        this._etag = etag !== undefined
-          ? Boolean(etag)
-          : true;
-      },
-      get etag(){
-        return this._etag;
-      },
-      // LastModified
-      set lastModified(lastModified){
-        this._lastModified = lastModified !== undefined
-          ? Boolean(lastModified)
-          : true;
-      },
-      get lastModified(){
-        return this._lastModified;
-      },
-      // MaxAge
-      set maxAge(maxAge){
-        // Max age
-        maxAge = util.isType(maxAge, 'string')
-          ? (ms(maxAge) || 0) / 1000
-          : Number(maxAge);
+      this._root = util.httpPath(root + path.sep);
+    },
+    get root(){
+      return this._root;
+    },
+    // Index
+    set index(index){
+      // Default document
+      this._index = index === false
+        ? []
+        : index === undefined || index === true
+        ? ['index.htm', 'index.html']
+        : util.normalizeList(index);
+    },
+    get index(){
+      return this._index;
+    },
+    // Extensions
+    set extensions(extensions){
+      this._extensions = !extensions
+        ? []
+        : util.normalizeList(extensions);
+    },
+    get extensions(){
+      return this._extensions;
+    },
+    // Etag
+    set etag(etag){
+      this._etag = etag !== undefined
+        ? Boolean(etag)
+        : true;
+    },
+    get etag(){
+      return this._etag;
+    },
+    // LastModified
+    set lastModified(lastModified){
+      this._lastModified = lastModified !== undefined
+        ? Boolean(lastModified)
+        : true;
+    },
+    get lastModified(){
+      return this._lastModified;
+    },
+    // MaxAge
+    set maxAge(maxAge){
+      // Max age
+      maxAge = util.isType(maxAge, 'string')
+        ? (ms(maxAge) || 0) / 1000
+        : Number(maxAge);
 
-        maxAge = maxAge >= 0
-          ? Math.min(maxAge, MAXMAXAGE)
-          : 0;
+      maxAge = maxAge >= 0
+        ? Math.min(maxAge, MAXMAXAGE)
+        : 0;
 
-        this._maxAge = Math.floor(maxAge);
-      },
-      get maxAge(){
-        return this._maxAge;
-      },
-      // DotFiles
-      set dotFiles(dotFiles){
-        // Dot files access, The value can be "allow", "deny", or "ignore"
-        this._dotFiles = util.isType(dotFiles, 'string')
-          ? dotFiles.toLowerCase()
-          : 'ignore';
-      },
-      get dotFiles(){
-        return this._dotFiles;
-      }
+      this._maxAge = Math.floor(maxAge);
+    },
+    get maxAge(){
+      return this._maxAge;
+    },
+    // DotFiles
+    set dotFiles(dotFiles){
+      // Dot files access, The value can be "allow", "deny", or "ignore"
+      this._dotFiles = util.isType(dotFiles, 'string')
+        ? dotFiles.toLowerCase()
+        : 'ignore';
+    },
+    get dotFiles(){
+      return this._dotFiles;
     }
   });
 
