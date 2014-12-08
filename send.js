4879d3d5706af91d84a2a54c439f697adfcb8cec
Nuintun <nuintun@qq.com>
2014-12-08 22:22:55 +0800

    add set method

--- send.js
+++ send.js
@@ -23,64 +23,32 @@ var ms = require('ms'), // Parse time string
  * @api public
  */
 function Send(root, options){
-  // Max age
-  var maxAge;
+  // Set other property
+  this.options = {};
 
   // Format options
-  options = util.mix({}, options);
+  options = options || {};
 
   // Root
-  root = util.isType(root, 'string')
-    ? path.resolve(root)
-    : cwd;
+  this._set_root(root);
 
-  root = util.httpPath(root + path.sep);
+  // ETag
+  this._set_etag(options.etag);
 
-  // Set root options
-  options.root = root;
-
-  // Etag
-  options.etag = options.etag !== undefined
-    ? Boolean(options.etag)
-    : true;
-
-  // Dot files access, The value can be "allow", "deny", or "ignore"
-  options.dotFiles = util.isType(options.dotFiles, 'string')
-    ? options.dotFiles.toLowerCase()
-    : 'ignore';
+  // dotFiles
+  this._set_dotFiles(options.dotFiles);
 
   // Extensions
-  options.extensions = !options.extensions
-    ? []
-    : util.normalizeList(options.extensions);
+  this._set_extensions(options.extensions);
 
   // Default document
-  options.index = options.index === false
-    ? []
-    : options.index === undefined || options.index === true
-    ? ['index.htm', 'index.html']
-    : util.normalizeList(options.index);
+  this._set_index(options.index);
 
   // Last modified
-  options.lastModified = options.lastModified !== undefined
-    ? Boolean(options.lastModified)
-    : true;
+  this._set_lastModified(options.lastModified);
 
   // Max age
-  maxAge = options.maxAge;
-
-  maxAge = util.isType(maxAge, 'string')
-    ? (ms(maxAge) || 0) / 1000
-    : Number(maxAge);
-
-  options.maxAge = maxAge >= 0
-    ? Math.min(maxAge, MAXMAXAGE)
-    : 0;
-
-  options.maxAge = Math.floor(options.maxAge);
-
-  // Set other property
-  this.options = options;
+  this._set_maxAge(options.maxAge);
 
   // Return instance
   return this;
@@ -97,8 +65,128 @@ Send.prototype.use = function (requset, response){
   return new SendStream(requset, response, this.options);
 };
 
+/**
+ * Set options
+ * @param key
+ * @param value
+ * @returns {Send}
+ */
 Send.prototype.set = function (key, value){
+  // Get method name
+  key = '_set_' + key;
 
+  // If method found, run it
+  this[key] && this[key](value);
+
+  // Return instance
+  return this;
+};
+
+/**
+ * Set root
+ * @private
+ */
+Send.prototype._set_root = function (root){
+  // Root
+  root = util.isType(root, 'string')
+    ? path.resolve(root)
+    : cwd;
+
+  root = util.httpPath(root + path.sep);
+
+  // Set root options
+  this.options.root = root;
+};
+
+/**
+ * Set Etag
+ * @private
+ */
+Send.prototype._set_etag = function (etag){
+  // Etag
+  etag = etag !== undefined
+    ? Boolean(etag)
+    : true;
+
+  // Set etag options
+  this.options.etag = etag;
+};
+
+/**
+ * Set dotFiles
+ * @private
+ */
+Send.prototype._set_dotFiles = function (dotFiles){
+  // Dot files access, The value can be "allow", "deny", or "ignore"
+  dotFiles = util.isType(dotFiles, 'string')
+    ? dotFiles.toLowerCase()
+    : 'ignore';
+
+  // Set dotFiles options
+  this.options.dotFiles = dotFiles;
+};
+
+/**
+ * Set extensions
+ * @private
+ */
+Send.prototype._set_extensions = function (extensions){
+  // Extensions
+  extensions = !extensions
+    ? []
+    : util.normalizeList(extensions);
+
+  // Set extensions options
+  this.options.extensions = extensions;
+};
+
+/**
+ * Set index
+ * @private
+ */
+Send.prototype._set_index = function (index){
+  // Default document
+  index = index === false
+    ? []
+    : index === undefined || index === true
+    ? ['index.htm', 'index.html']
+    : util.normalizeList(index);
+
+  // Set index options
+  this.options.index = index;
+};
+/**
+ * Set lastModified
+ * @private
+ */
+Send.prototype._set_lastModified = function (lastModified){
+  // Last modified
+  lastModified = lastModified !== undefined
+    ? Boolean(lastModified)
+    : true;
+
+  // Set lastModified options
+  this.options.lastModified = lastModified;
+};
+
+/**
+ * Set maxAge
+ * @private
+ */
+Send.prototype._set_maxAge = function (maxAge){
+  // Max age
+  maxAge = util.isType(maxAge, 'string')
+    ? (ms(maxAge) || 0) / 1000
+    : Number(maxAge);
+
+  maxAge = maxAge >= 0
+    ? Math.min(maxAge, MAXMAXAGE)
+    : 0;
+
+  maxAge = Math.floor(maxAge);
+
+  // Set maxAge options
+  this.options.maxAge = maxAge;
 };
 
 // Expose mime
