67a827208b857faed0da2a085ab08dba8f6cc102
nuintun <nuintun@qq.com>
2016-06-22 15:28:59 +0800

    update deps

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ var path = require('path');
 var http = require('http');
 var util = require('./lib/util');
 var micromatch = require('micromatch');
+var through = require('./lib/through');
 var EventEmitter = require('events').EventEmitter;
 
 // variable declaration
@@ -39,21 +40,21 @@ function FileSend(request, options){
 
   options = options || {};
 
-  var __root, __etag, __ignore, __ignoreAccess, __maxAge, __lastModified;
+  var root, etag, ignore, ignoreAccess, maxAge, lastModified;
 
   // root
   util.defineProperty(this, 'root', {
     enumerable: true,
     get: function (){
-      if (!__root) {
-        __root = util.isType(options.root, 'string')
+      if (!root) {
+        root = util.isType(options.root, 'string')
           ? path.resolve(options.root)
           : CWD;
 
-        __root = util.posixPath(__root + path.sep);
+        root = util.posixPath(root + path.sep);
       }
 
-      return __root;
+      return root;
     }
   });
 
@@ -61,13 +62,13 @@ function FileSend(request, options){
   util.defineProperty(this, 'etag', {
     enumerable: true,
     get: function (){
-      if (!__etag) {
-        __etag = options.etag !== undefined
+      if (!etag) {
+        etag = options.etag !== undefined
           ? Boolean(options.etag)
           : true;
       }
 
-      return __etag;
+      return etag;
     }
   });
 
@@ -75,23 +76,23 @@ function FileSend(request, options){
   util.defineProperty(this, 'ignore', {
     enumerable: true,
     get: function (){
-      if (!__ignore) {
+      if (!ignore) {
         if (this.ignoreAccess !== 'allow') {
-          __ignore = Array.isArray(options.ignore)
+          ignore = Array.isArray(options.ignore)
             ? options.ignore
             : [options.ignore];
 
-          __ignore = __ignore.filter(function (pattern){
+          ignore = ignore.filter(function (pattern){
             return util.isType(pattern, 'string')
               || util.isType(pattern, 'regexp')
               || util.isType(pattern, 'function');
           });
         } else {
-          __ignore = [];
+          ignore = [];
         }
       }
 
-      return __ignore;
+      return ignore;
     }
   });
 
@@ -99,19 +100,19 @@ function FileSend(request, options){
   util.defineProperty(this, 'ignoreAccess', {
     enumerable: true,
     get: function (){
-      if (!__ignoreAccess) {
+      if (!ignoreAccess) {
         switch (options.ignoreAccess) {
           case 'allow':
           case 'deny':
           case 'ignore':
-            __ignoreAccess = options.ignoreAccess;
+            ignoreAccess = options.ignoreAccess;
             break;
           default:
-            __ignoreAccess = 'deny';
+            ignoreAccess = 'deny';
         }
       }
 
-      return __ignoreAccess;
+      return ignoreAccess;
     }
   });
 
@@ -119,19 +120,19 @@ function FileSend(request, options){
   util.defineProperty(this, 'maxAge', {
     enumerable: true,
     get: function (){
-      if (!__maxAge) {
-        __maxAge = util.isType(options.maxAge, 'string')
+      if (!maxAge) {
+        maxAge = util.isType(options.maxAge, 'string')
           ? (ms(options.maxAge) || 0) / 1000
           : Number(options.maxAge);
 
-        __maxAge = __maxAge >= 0
-          ? Math.min(__maxAge, MAXMAXAGE)
+        maxAge = maxAge >= 0
+          ? Math.min(maxAge, MAXMAXAGE)
           : MAXMAXAGE;
 
-        __maxAge = Math.floor(__maxAge);
+        maxAge = Math.floor(maxAge);
       }
 
-      return __maxAge;
+      return maxAge;
     }
   });
 
@@ -139,31 +140,60 @@ function FileSend(request, options){
   util.defineProperty(this, 'lastModified', {
     enumerable: true,
     get: function (){
-      if (!__lastModified) {
-        __lastModified = options.lastModified !== undefined
+      if (!lastModified) {
+        lastModified = options.lastModified !== undefined
           ? Boolean(options.lastModified)
           : true;
       }
 
-      return __lastModified;
+      return lastModified;
     }
   });
+
+  // stream
+  util.defineProperty(this, 'stream', {
+    value: null,
+    writable: true,
+    enumerable: false
+  });
+
+  this.initialize();
 }
 
-FileSend.prototype = Object.create(EventEmitter.prototype, { constructor: { value: FileSend } });
+FileSend.prototype = Object.create(EventEmitter.prototype, {
+  constructor: { value: FileSend }
+});
+
+FileSend.prototype.initialize = function (){
+  this.stream = through();
+  this.stream.push('hello world !');
+  this.stream.end();
+};
+
+FileSend.prototype.pipe = function (response){
+  if (response instanceof http.OutgoingMessage) {
+    response.writeHead(this.statusCode, this.statusMessage, this.headers);
+  }
+
+  this.stream = this.stream.pipe(response);
+
+  return this;
+};
 
 http.createServer(function (request, response){
   var send = new FileSend(request, {
     ignore: ['*.js']
   });
 
-  console.log(send.root);
-  console.log(send.etag);
-  console.log(send.ignore);
-  console.log(send.ignoreAccess);
-  console.log(send.maxAge);
-  console.log(send.lastModified);
-  console.log(micromatch.any('a.js', send.ignore));
+  // console.log(send.root);
+  // console.log(send.etag);
+  // console.log(send.ignore);
+  // console.log(send.ignoreAccess);
+  // console.log(send.maxAge);
+  // console.log(send.lastModified);
+  // console.log(micromatch.any('a.js', send.ignore));
 
-  response.end('FileSend');
+  var stream = through();
+
+  send.pipe(stream).pipe(response);
 }).listen(9091, '127.0.0.1');
