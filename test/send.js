c0ae5f641f065a7649e52f1da4d5ca19b4aefb7c
nuintun <nuintun@qq.com>
2016-06-28 16:30:52 +0800

    update files

--- test/send.js
+++ test/send.js
@@ -744,7 +744,7 @@ describe('Options', function (){
     it('should default to 0', function (done){
       request(app)
         .get('/name.txt')
-        .expect('Cache-Control', 'public, private', done);
+        .expect('Cache-Control', 'private', done);
     });
 
     it('should floor to integer', function (done){
