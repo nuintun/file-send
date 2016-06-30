72882050194915e0c3be87a5ef748f6358377cab
nuintun <nuintun@qq.com>
2016-06-30 15:30:52 +0800

    update files

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ const numCPUs = require('os').cpus().length;
 function createServer(root, port){
   http.createServer(function (request, response){
     var send = new FileSend(request, {
-      query: true,
       root: root || './',
       maxAge: '3day',
       ignore: ['/**/.*?(/*.*|/)'],
@@ -23,7 +22,6 @@ function createServer(root, port){
     send.pipe(response).on('headers', function (response, headers){
       console.log('ROOT     :', colors.green.bold(send.root));
       console.log('URL      :', colors.magenta.bold(send.url));
-      console.log('QUERY    :', colors.magenta.bold(JSON.stringify(send.query, null, 2)));
       console.log('PATH     :', colors.yellow.bold(send.path));
       console.log('REALPATH :', colors.yellow.bold(send.realpath));
       console.log('STATUS   :', colors.cyan.bold(send.statusCode));
