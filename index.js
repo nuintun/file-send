22c907da10056fb4f717ae0714f070ef57eec902
nuintun <nuintun@qq.com>
2016-09-06 10:17:41 +0800

    add cache-control parse

--- index.js
+++ index.js
@@ -503,13 +503,27 @@ FileSend.prototype.setHeaders = function (response, stats){
   }
 
   // set cache-control
-  if (cacheControl) {
+  if (cacheControl !== undefined) {
     context.setHeader('Cache-Control', cacheControl);
   } else {
     cacheControl = context.request.headers['cache-control'];
 
-    if (context.maxAge > 0 && cacheControl !== 'no-cache') {
-      context.setHeader('Cache-Control', 'public, max-age=' + context.maxAge);
+    var cacheControlObject = util.parseCacheControl(cacheControl);
+
+    if (cacheControlObject !== null) {
+      cacheControl = '';
+
+      for (var key in cacheControlObject) {
+        if (key !== 'max-age' && cacheControlObject.hasOwnProperty(key)) {
+          cacheControl += (cacheControl.length ? ', ' : '') + key;
+        }
+      }
+
+      if (cacheControlObject.hasOwnProperty('max-age') && context.maxAge > 0) {
+        cacheControl += (cacheControl.length ? ', ' : '') + 'max-age=' + context.maxAge;
+
+        context.setHeader('Cache-Control', cacheControl);
+      }
     }
   }
 
