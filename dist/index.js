7798e9ed617e49503cb8f65f73cee42a53174466
nuintun <nuintun@qq.com>
2017-10-27 18:21:17 +0800

    :hammer: rewrite

--- dist/index.js
+++ dist/index.js
@@ -2,8 +2,6 @@
 
 require('ms');
 require('fs');
-require('url');
-require('path');
 var http = require('http');
 require('etag');
 require('fresh');
@@ -15,6 +13,7 @@ require('micromatch');
 require('on-finished');
 require('escape-html');
 require('range-parser');
+var path = require('path');
 
 /**
  * @module utils
@@ -22,6 +21,8 @@ require('range-parser');
  * @version 2017/10/24
  */
 
+const undef = void(0);
+const toString = Object.prototype.toString;
 const CHARS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
 
 /**
@@ -31,7 +32,28 @@ const CHARS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.s
  * @param {string} type
  * @returns {boolean}
  */
+function typeIs(value, type) {
+  // Format type
+  type = (type + '').toLowerCase();
 
+  // Is array
+  if (type === 'array') {
+    return Array.isArray(value);
+  }
+
+  // Get real type
+  const realType = toString.call(value).toLowerCase();
+
+  // Switch
+  switch (type) {
+    case 'nan':
+      // Is nan
+      return realType === '[object number]' && value !== value;
+    default:
+      // Is other
+      return realType === '[object ' + type + ']';
+  }
+}
 
 /**
  * @function isOutBound
@@ -48,7 +70,43 @@ const CHARS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.s
  * @param {string} path
  * @returns {string}
  */
+function normalize(path$$1) {
+  // \a\b\.\c\.\d ==> /a/b/./c/./d
+  path$$1 = path$$1.replace(/\\/g, '/');
 
+  // :///a/b/c ==> ://a/b/c
+  path$$1 = path$$1.replace(/(:)?\/{2,}/, '$1//');
+
+  // /a/b/./c/./d ==> /a/b/c/d
+  path$$1 = path$$1.replace(/\/\.\//g, '/');
+
+  // @author wh1100717
+  // a//b/c ==> a/b/c
+  // a///b/////c ==> a/b/c
+  path$$1 = path$$1.replace(/([^:/])\/+\//g, '$1/');
+
+  // Transfer path
+  let src = path$$1;
+  // DOUBLE_DOT_RE matches a/b/c//../d path correctly only if replace // with / first
+  const DOUBLE_DOT_RE = /([^/]+)\/\.\.(?:\/|$)/g;
+
+  // a/b/c/../../d ==> a/b/../d ==> a/d
+  do {
+    src = src.replace(DOUBLE_DOT_RE, function(matched, dirname) {
+      return dirname === '..' ? matched : '';
+    });
+
+    // Break
+    if (path$$1 === src) {
+      break;
+    } else {
+      path$$1 = src;
+    }
+  } while (true);
+
+  // Get path
+  return path$$1;
+}
 
 /**
  * @function posixURI
@@ -64,7 +122,13 @@ const CHARS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.s
  * @param {string} uri
  * @returns {string|-1}
  */
-
+function decodeURI(uri) {
+  try {
+    return decodeURIComponent(uri);
+  } catch (err) {
+    return -1;
+  }
+}
 
 /**
  * @function boundaryGenerator
@@ -90,6 +154,52 @@ const CHARS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.s
  * @see https://github.com/micro-js/apply
  * @see http://blog.csdn.net/zhengyinhui100/article/details/7837127
  */
+
+
+/**
+ * @function isUndefined
+ * @param {any} value
+ */
+function isUndefined(value) {
+  return value === undef;
+}
+
+/**
+ * @function setHeaders
+ * @param {Response} response
+ * @param {Object} headers
+ */
+function setHeaders(response, headers) {
+  Object
+    .keys(headers)
+    .forEach(function(key) {
+      response.setHeader(key, headers[key]);
+    });
+}
+
+/**
+ * @function pipeline
+ * @param {Stream} streams
+ * @return {Stream}
+ */
+function pipeline(streams) {
+  let stream;
+  const length = streams.length;
+
+  if (length > 1) {
+    let index = 0;
+
+    stream = streams[index++];
+
+    while (index < length) {
+      stream = stream.pipe(streams[index++]);
+    }
+  } else {
+    stream = streams[0];
+  }
+
+  return stream;
+}
 
 /**
  * @module async
@@ -131,39 +241,124 @@ const CHARS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.s
  * @returns {DestroyableTransform}
  */
 
+// Current working directory
+const CWD = process.cwd();
+// The max max-age set
+const MAX_MAX_AGE = 60 * 60 * 24 * 365;
+
+function normalizeCharset(charset) {
+  return util.typeIs(charset, 'string') ? charset : null;
+}
+
+function normalizeRoot(root) {
+  return util.posixURI(util.typeIs(root, 'string') ? path.resolve(root) : CWD);
+}
+
+function normalizeList(list) {
+  list = Array.isArray(list) ? list : [list];
+
+  return list.filter(function(item) {
+    return item && typeIs(item, 'string');
+  })
+}
+
+function normalizeAccess(access) {
+  switch (access) {
+    case 'deny':
+    case 'ignore':
+      return access;
+      break;
+    default:
+      return 'deny';
+  }
+}
+
+function normalizePath(path$$1) {
+  path$$1 = decodeURI(path$$1);
+
+  return path$$1 === -1 ? path$$1 : normalize(path$$1);
+}
+
+function normalizeMaxAge(maxAge) {
+  maxAge = typeIs(maxAge, 'string') ? ms(maxAge) / 1000 : Number(maxAge);
+  maxAge = !isNaN(maxAge) ? Math.min(Math.max(0, maxAge), MAX_MAX_AGE) : 0;
+
+  return Math.floor(maxAge);
+}
+
+function normalizeBoolean(boolean, def) {
+  return isUndefined(boolean) ? def : Boolean(boolean);
+}
+
 /**
  * @module file-send
  * @license MIT
  * @version 2017/10/25
  */
 
-// Current working directory
-const CWD = process.cwd();
-const originWriteHead = http.ServerResponse.prototype.writeHead;
-
-// Add http response write headers events
-http.ServerResponse.prototype.writeHead = function() {
-  // Emit headers event
-  if (this.listenerCount('headers') > 0) {
-    this.emit('headers');
-  }
-
-  // Call origin method
-  util.apply(originWriteHead, this, arguments);
-};
+// Headers key symbol
+const headers = Symbol('headers');
+const middlewares = Symbol('middlewares');
 
 /**
  * @class FileSend
  */
-
 class FileSend extends Stream {
   /**
    * @constructor
    * @param {Request} request
+   * @param {String} path
    * @param {Object} options
    */
   constructor(request, path$$1, options) {
+    if (!(request instanceof http.IncomingMessage)) {
+      throw new TypeError('The param request must be a http request.');
+    }
+
+    if (!typeIs(path$$1, 'string')) {
+      throw new TypeError('The param path must be a string.');
+    }
+
     super();
+
+    this[headers] = {};
+    this[middlewares] = [];
+    this.request = request;
+    this.method = request.method;
+    this.path = normalizePath(path$$1);
+    this.root = normalizeRoot(options.root);
+    this.index = normalizeList(options.index);
+    this.ignore = normalizeList(options.ignore);
+    this.maxAge = normalizeMaxAge(options.maxAge);
+    this.charset = normalizeCharset(options.charset);
+    this.etag = normalizeBoolean(options.etag, true);
+    this.ignoreAccess = normalizeAccess(options.ignoreAccess);
+    this.immutable = normalizeBoolean(options.immutable, false);
+    this.acceptRanges = normalizeBoolean(options.acceptRanges, true);
+    this.cacheControl = normalizeBoolean(options.cacheControl, true);
+    this.lastModified = normalizeBoolean(options.lastModified, true);
+  }
+
+  use(middleware) {
+    if (middleware instanceof Stream.Transform) {
+      this[middlewares].push(middleware);
+    }
+  }
+
+  setHeader(name, value) {
+    this[headers][name.toLowerCase()] = value;
+  }
+
+  getHeader(name) {
+    return this[headers][name.toLowerCase()];
+  }
+
+  getHeaders() {
+    return this[headers];
+  }
+
+  removeHeader(name) {
+    delete this[headers][name.toLowerCase()];
   }
 
   hasListeners(event) {
@@ -171,10 +366,83 @@ class FileSend extends Stream {
   }
 
   headersSent(response) {
-    if (response.headersSent) {
-      this.unpipe(response);
-      response.end('Can\'t set headers after they are sent.');
+    response.end('Can\'t set headers after they are sent.');
+  }
+
+  status(response, statusCode, statusMessage) {
+    this.statusCode = statusCode;
+    this.statusMessage = statusMessage || http.STATUS_CODES[statusCode];
+
+    response.statusCode = this.statusCode;
+    response.statusMessage = this.statusMessage;
+  }
+
+  error(response, statusCode, statusMessage) {
+    this.status(response, statusCode, statusMessage);
+
+    statusCode = this.statusCode;
+    statusMessage = this.statusMessage;
+
+    const error = new Error(statusMessage);
+
+    error.statusCode = statusCode;
+
+    // Emit if listeners instead of responding
+    if (this.hasListeners('error')) {
+      this.emit('error', error, (message) => {
+        if (response.headersSent) {
+          return this.headersSent(response);
+        }
+
+        response.end(message);
+      });
+    } else {
+      // Set headers
+      this.setHeader('Cache-Control', 'private');
+      this.setHeader('Content-Type', 'text/html; charset=UTF-8');
+      this.setHeader('Content-Length', Buffer.byteLength(statusMessage));
+      this.setHeader('Content-Security-Policy', "default-src 'self'");
+      this.setHeader('X-Content-Type-Options', 'nosniff');
     }
+  }
+
+  statError() {
+
+  }
+
+  hasTrailingSlash() {
+    return this.path[this.path.length - 1] === '/';
+  }
+
+  isConditionalGET() {
+    const headers = this.request.headers;
+
+    return headers['if-match']
+      || headers['if-unmodified-since']
+      || headers['if-none-match']
+      || headers['if-modified-since'];
+  }
+
+  pipe(response, options) {
+    if (!response instanceof http.ServerResponse) {
+      throw new TypeError('The param response must be a http response.')
+    }
+
+    if (response.headersSent) {
+      this.headersSent(response);
+
+      return response;
+    }
+
+    setHeaders(response, this.getHeaders());
+
+    const streams = [this];
+
+    streams.concat(this[middlewares]);
+
+    streams.push(response);
+
+    return pipeline(streams);
   }
 }
 
--- index.js
+++ index.js
@@ -6,8 +6,6 @@
 
 import * as ms from 'ms';
 import * as fs from 'fs';
-import * as url from 'url';
-import * as path from 'path';
 import * as http from 'http';
 import * as etag from 'etag';
 import * as fresh from 'fresh';
@@ -15,49 +13,104 @@ import * as Stream from 'stream';
 import * as destroy from 'destroy';
 import * as mime from 'mime-types';
 import * as utils from './lib/utils';
-import * as async from './lib/async';
 import * as encodeUrl from 'encodeurl';
-import { through } from './lib/through';
 import * as micromatch from 'micromatch';
 import * as onFinished from 'on-finished';
 import * as escapeHtml from 'escape-html';
 import * as parseRange from 'range-parser';
 
-// Current working directory
-const CWD = process.cwd();
-// The max max-age set
-const MAXMAXAGE = 60 * 60 * 24 * 365;
+import {
+  series
+} from './lib/async';
+
+import {
+  through
+} from './lib/through';
+
+import {
+  join,
+  resolve
+} from 'path';
+
+import {
+  normalizeRoot,
+  normalizePath,
+  normalizeList,
+  normalizeBoolean,
+  normalizeCharset,
+  normalizeAccess,
+  normalizeMaxAge,
+  normalizeGlob
+} from './lib/normalize';
+
+// Headers key symbol
+const glob = Symbol('glob');
+const headers = Symbol('headers');
+const middlewares = Symbol('middlewares');
+
 // File not found status
-const NOTFOUND = ['ENOENT', 'ENAMETOOLONG', 'ENOTDIR'];
-
-// Common method
-const join = path.join;
-const resolve = path.resolve;
-const originWriteHead = http.ServerResponse.prototype.writeHead;
-
-// Add http response write headers events
-http.ServerResponse.prototype.writeHead = function() {
-  // Emit headers event
-  if (this.listenerCount('headers') > 0) {
-    this.emit('headers');
-  }
-
-  // Call origin method
-  util.apply(originWriteHead, this, arguments);
-};
+const NOT_FOUND = ['ENOENT', 'ENAMETOOLONG', 'ENOTDIR'];
 
 /**
  * @class FileSend
  */
-
 export default class FileSend extends Stream {
   /**
    * @constructor
    * @param {Request} request
+   * @param {String} path
    * @param {Object} options
    */
   constructor(request, path, options) {
+    if (!(request instanceof http.IncomingMessage)) {
+      throw new TypeError('The param request must be a http request.');
+    }
+
+    if (!utils.typeIs(path, 'string')) {
+      throw new TypeError('The param path must be a string.');
+    }
+
     super();
+
+    this[headers] = {};
+    this[middlewares] = [];
+    this.request = request;
+    this.method = request.method;
+    this.path = normalizePath(path);
+    this.root = normalizeRoot(options.root);
+    this[glob] = normalizeGlob(options.glob);
+    this.index = normalizeList(options.index);
+    this.ignore = normalizeList(options.ignore);
+    this.maxAge = normalizeMaxAge(options.maxAge);
+    this.charset = normalizeCharset(options.charset);
+    this.etag = normalizeBoolean(options.etag, true);
+    this.ignoreAccess = normalizeAccess(options.ignoreAccess);
+    this.immutable = normalizeBoolean(options.immutable, false);
+    this.acceptRanges = normalizeBoolean(options.acceptRanges, true);
+    this.cacheControl = normalizeBoolean(options.cacheControl, true);
+    this.lastModified = normalizeBoolean(options.lastModified, true);
+  }
+
+  use(middleware) {
+    if (middleware instanceof Stream.Transform) {
+      this[middlewares].push(middleware);
+    }
+  }
+
+  setHeader(name, value) {
+    this[headers][name.toLowerCase()] = value;
+  }
+
+  getHeader(name) {
+    return this[headers][name.toLowerCase()];
+  }
+
+  getHeaders() {
+    return this[headers];
+  }
+
+  removeHeader(name) {
+    delete this[headers][name.toLowerCase()];
   }
 
   hasListeners(event) {
@@ -65,9 +118,179 @@ export default class FileSend extends Stream {
   }
 
   headersSent(response) {
-    if (response.headersSent) {
-      this.unpipe(response);
-      response.end('Can\'t set headers after they are sent.');
+    response.end('Can\'t set headers after they are sent.');
+  }
+
+  status(response, statusCode, statusMessage) {
+    this.statusCode = statusCode;
+    this.statusMessage = statusMessage || http.STATUS_CODES[statusCode];
+
+    response.statusCode = this.statusCode;
+    response.statusMessage = this.statusMessage;
+  }
+
+  error(response, statusCode, statusMessage) {
+    this.status(response, statusCode, statusMessage);
+
+    statusCode = this.statusCode;
+    statusMessage = this.statusMessage;
+
+    const error = new Error(statusMessage);
+
+    error.statusCode = statusCode;
+
+    // Emit if listeners instead of responding
+    if (this.hasListeners('error')) {
+      this.emit('error', error, (message) => {
+        if (response.headersSent) {
+          return this.headersSent(response);
+        }
+
+        response.end(message);
+      });
+    } else {
+      // Set headers
+      this.setHeader('Cache-Control', 'private');
+      this.setHeader('Content-Type', 'text/html; charset=UTF-8');
+      this.setHeader('Content-Length', Buffer.byteLength(statusMessage));
+      this.setHeader('Content-Security-Policy', "default-src 'self'");
+      this.setHeader('X-Content-Type-Options', 'nosniff');
     }
+  }
+
+  statError(response, error) {
+    // 404 error
+    if (NOTFOUND.indexOf(error.code) !== -1) {
+      return this.error(response, 404);
+    }
+
+    this.error(response, 500, error.message);
+  }
+
+  hasTrailingSlash() {
+    return this.path[this.path.length - 1] === '/';
+  }
+
+  isConditionalGET() {
+    const headers = this.request.headers;
+
+    return headers['if-match']
+      || headers['if-unmodified-since']
+      || headers['if-none-match']
+      || headers['if-modified-since'];
+  }
+
+  isPreconditionFailure(response) {
+    const request = this.request;
+    // if-match
+    const match = request.headers['if-match'];
+
+    if (match) {
+      const etag = response.getHeader('ETag');
+
+      return !etag || (match !== '*' && utils.parseTokenList(match).every(function(match) {
+        return match !== etag && match !== 'W/' + etag && 'W/' + match !== etag;
+      }));
+    }
+
+    // if-unmodified-since
+    const unmodifiedSince = utils.parseHttpDate(request.headers['if-unmodified-since']);
+
+    if (!isNaN(unmodifiedSince)) {
+      const lastModified = utils.parseHttpDate(response.getHeader('Last-Modified'));
+
+      return isNaN(lastModified) || lastModified > unmodifiedSince;
+    }
+
+    return false;
+  }
+
+  isCachable() {
+    const statusCode = this.statusCode;
+
+    return statusCode === 304 || (statusCode >= 200 && statusCode < 300);
+  }
+
+  isFresh() {
+    return fresh(this.request.headers, {
+      'etag': this.getHeader('ETag'),
+      'last-modified': this.getHeader('Last-Modified')
+    });
+  }
+
+  isRangeFresh() {
+    const ifRange = this.request.headers['if-range'];
+
+    if (!ifRange) {
+      return true;
+    }
+
+    // if-range as etag
+    if (ifRange.indexOf('"') !== -1) {
+      const etag = this.getHeader('ETag');
+
+      return Boolean(etag && ifRange.indexOf(etag) !== -1);
+    }
+
+    // if-range as modified date
+    const lastModified = this.getHeader('Last-Modified');
+
+    return utils.parseHttpDate(lastModified) <= utils.parseHttpDate(ifRange);
+  }
+
+  isIgnore(path) {
+    return this.ignore.length && micromatch(path, this.ignore, this[glob]).length;
+  }
+
+  dir(response, realpath, stats) {
+    // If have event directory listener, use user define
+    // emit event directory
+    if (this.hasListeners('dir')) {
+      this.emit('dir', realpath, stats, (message) => {
+        if (response.headersSent) {
+          return this.headersSent(response);
+        }
+
+        response.end(message);
+      });
+    } else {
+      this.error(response, 403);
+    }
+  }
+
+  redirect(response, location) {
+    const message = 'Redirecting to <a href="' + location + '">' + escapeHtml(location) + '</a>';
+
+    this.status(response, 301);
+    this.setHeader('Cache-Control', 'no-cache');
+    this.setHeader('Content-Type', 'text/html; charset=UTF-8');
+    this.setHeader('Content-Length', Buffer.byteLength(message));
+    this.setHeader('Content-Security-Policy', "default-src 'self'");
+    this.setHeader('X-Content-Type-Options', 'nosniff');
+    this.setHeader('Location', location);
+
+    response.end(message);
+  }
+
+  pipe(response, options) {
+    if (!response instanceof http.ServerResponse) {
+      throw new TypeError('The param response must be a http response.')
+    }
+
+    if (response.headersSent) {
+      this.headersSent(response);
+
+      return response;
+    }
+
+    utils.setHeaders(response, this.getHeaders());
+
+    const streams = [this];
+
+    streams.concat(this[middlewares]);
+
+    streams.push(response);
+
+    return utils.pipeline(streams);
   }
 }
