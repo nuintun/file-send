21d4279760c57396d57181bcb1d070bffd46bcbb
nuintun <nuintun@qq.com>
2019-05-14 17:41:21 +0800

    :arrow_up: Bump deps

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @module file-send
  * @author nuintun
  * @license MIT
- * @version 4.0.0
+ * @version 4.0.1
  * @description A http file send.
  * @see https://github.com/nuintun/file-send#readme
  */
